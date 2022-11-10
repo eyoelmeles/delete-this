44a270caa91d274855e9584a5e193320ce74aecf
eyoelmeles <tojoelmeles@gmail.com>
2022-11-10 00:43:44 -0800

    custom-table

--- src/component/users-table/index.tsx
+++ src/component/users-table/index.tsx
@@ -10,7 +10,7 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
 interface Column {
-  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  id: 'fullname' | 'username' | 'email' | 'phoneno';
   label: string;
   minWidth?: number;
   align?: 'right';
@@ -18,68 +18,53 @@ interface Column {
 }
 
 const columns: Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'fullname', label: 'Full Name', minWidth: 170 },
+  { id: 'username', label: 'Username', minWidth: 100 },
   {
-    id: 'population',
-    label: 'Population',
+    id: 'email',
+    label: 'Email',
     minWidth: 170,
     align: 'right',
     format: (value: number) => value.toLocaleString('en-US'),
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
+    id: 'phoneno',
+    label: 'Phone Number',
     minWidth: 170,
     align: 'right',
     format: (value: number) => value.toLocaleString('en-US'),
   },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2),
-  },
+  // {
+  //   id: 'density',
+  //   label: 'Density',
+  //   minWidth: 170,
+  //   align: 'right',
+  //   format: (value: number) => value.toFixed(2),
+  // },
 ];
 
 interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
+  fullname: string;
+  username: string;
+  email: string;
+  phoneno: string;
 }
 
 function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number,
+  fullname: string,
+  username: string,
+  email: string,
+  phoneno: string,
 ): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
+  return { fullname, username, email, phoneno };
 }
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+
 
 export default function UsersTable({ users }) {
+
+	const rows = users.map(user => createData(user.fullname, user.username, user.email, user.phoneno));
+
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
@@ -97,14 +82,14 @@ export default function UsersTable({ users }) {
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
-            <TableRow>
+            {/* <TableRow>
               <TableCell align="center" colSpan={2}>
                 Country
               </TableCell>
               <TableCell align="center" colSpan={3}>
                 Details
               </TableCell>
-            </TableRow>
+            </TableRow> */}
             <TableRow>
               {columns.map((column) => (
                 <TableCell
@@ -120,9 +105,9 @@ export default function UsersTable({ users }) {
           <TableBody>
             {rows
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
+              .map((row: Data, index: number) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={`row.username-${index}`}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
